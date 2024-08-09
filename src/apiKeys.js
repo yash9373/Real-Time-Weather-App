1ffabaccc3270c459e04e9d373ea8423049a900c
Yashodip Kolhe <kolheyashodip8@gmail.com>
2024-08-09 08:50:53 +0530

    Initial commit - Adding Real-Time Weather App code

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "571e2b210a3f6ddd3ea99867a768fa6d",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
